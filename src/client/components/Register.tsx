02011b31e3e0efad3bb6229eedbf898ca5d26686
Ian <ianr.hnizdo@gmail.com>
2023-02-07 16:04:10 -0500

    Improved Registration Functionality.

--- src/client/components/Register.tsx
+++ src/client/components/Register.tsx
@@ -102,31 +102,96 @@ function Register(): JSX.Element{
     }
     if(forgotPass){
         return(
-            <section className="Registration">
-                <p ref={errRef} className={errMsg ? 'errrmsg' : 'offscreen'} aria-live="assertive">{errMsg}</p>
-                <h1>Register</h1>
-                <form className="Reset_Pass" onSubmit={check}>
-                    <label htmlFor='username'>What is your username?</label>
-                    <input type='text' name='username' id='username' required/>
-                    <label htmlFor='password1'>Put in your password, must be between 8-16 characters.</label>
-                    <input type='password' name='password1' id='password1' minLength={8} maxLength={16} required/>
-                    <label htmlFor='password2'>Retype your password</label>
-                    <input type='password' name='password2' id='password2' minLength={8} maxLength={16} required/>
-                    <button type='submit'>Submit</button>
+            <section className="Forgot Password">
+                <form className="Pass">
                 </form>
             </section>            
         )
     }
     else return(
-        <div className="Registration">
-            <form className="Register_Form" onSubmit={check}>
-                <input type='text' name='username' id='username' required/>
-                <input type='password' name='password1' id='password1' minLength={8} maxLength={16} required/>
-                <input type='password' name='password2' id='password2' minLength={8} maxLength={16} required/>
-                <button type='submit'>Submit</button>
-            </form>
-            <button onClick={()=>setForgotPass(true)}>Forgot Password</button>
-        </div>
+        <section className="Registration">
+        <p ref={errRef} className={errMsg ? 'errrmsg' : 'offscreen'} aria-live="assertive">{errMsg}</p>
+        <h1>Register</h1>
+        <form className="Register" onSubmit={check}>
+
+            <label htmlFor='username'>What is your username?
+                <span className={validName ? "valid" : "hide"}>
+                    <FontAwesomeIcon icon={faCheck}/>
+                </span>
+                {/* We have !user since we don't want to show the 'X' until the user has enetered data */}
+                <span className={validName || !user ? "hide" : "valid"}>
+                    <FontAwesomeIcon icon={faTimes}/>
+                </span>
+            </label>
+
+            {/* aria-invalid, is our input valid or not. aria-described by, full description. Screenreader will read them.  */}
+            <input type='text' name='username' id='username' ref={userRef} autoComplete="off" onChange={(e)=>setUser(e.target.value)} required aria-invalid={validName ? "false" : "true"} aria-describedby="uidnote" onFocus={()=>setUserFocus(true)} onBlur={()=>setUserFocus(false)}/>
+
+            {/* uid note, what is this? Is our userFocus is true on the input, make sure that they have typed into user, and if the username isn't valid keep the instrucitons, otherwise hide them. This className is linked with the CSS to make it displayed  */}
+            <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                <FontAwesomeIcon icon={faInfoCircle}/>
+                4 to 24 characters <br />
+                Must begin with a letter. <br />
+                Letters, numbers, underscores, hyphens, allowed.
+            </p>
+
+            <label htmlFor='password1'>Put in your password, must be between 8-16 characters.
+                <span className={validPwd ? "valid": "hide"}>
+                    <FontAwesomeIcon icon={faCheck}/>
+                </span>
+
+                <span className={validPwd || !pwd ? "hide" : "valid"}>
+                    <FontAwesomeIcon icon={faTimes}/>
+                </span>
+            </label>
+
+            {/* One easy way to force user passwords is to just put in minLength and maxLength parameters. There is no autocomplete here since type"password" doesn't support it anyway. There is no reference here either as we don't want to set the focus on this field when it loads.*/}
+            <input type='password' name='password1' id='password1' onChange={(e)=>setPwd(e.target.value)}  required aria-invalid={validPwd ? "false" : "true"} aria-describedbhy="pwdnote" onFocus={()=> setPwdFocus(true)} onBlur={()=>setPwdFocus(false)}/>
+            
+            <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    8 to 24 characters. <br />
+                    Must include uppercase and lowercase letters, a number and a special character. <br />
+                    {/* Okay what the hack is all the spans. They are for the screen reader. The aria-label attribute allows us the screen reader to read each special character. */}
+                    Allowed special characters: <span aria-label="exclamation mark">!</span>
+                    <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span><span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+            </p>
+            
+            <label htmlFor='password2'>Retype your password
+                <span className={validMatch && matchPwd ? 'valid': 'hide'}>
+                    <FontAwesomeIcon icon={faCheck}/>
+                </span>
+                <span className={validMatch || !matchPwd ? 'hide' : 'valid'}>
+                    <FontAwesomeIcon icon={faTimes}/>
+                </span>
+            </label>
+            <input type='password' name='password2' id='password2' onChange={(e)=>setMachPwd(e.target.value)} aria-invalid={validMatch ? 'false' : 'true'} aria-describedby={'confirmnote'} required onFocus={()=>setMatchFocus(true)} onBlur={()=>setMatchFocus(false)}/>
+            <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                <FontAwesomeIcon icon={faInfoCircle} />
+                Must match the first password input field.
+            </p>
+
+            {/* Keep the button disabled until everything is validated */}
+            <button disabled={!validName || !validPwd || !validMatch ? true : false}>Submit and get signed up!</button>
+        </form>
+        <p>
+            Already registered?<br />
+            <span className="line">
+                {/* put router link here */}
+                {/* <a href="#">Sign In</a> */}
+            </span>
+        </p>
+    </section>            
+
+        // <div className="Registration">
+        //     <form className="Register_Form" onSubmit={check}>
+        //         <input type='text' name='username' id='username' required/>
+        //         <input type='password' name='password1' id='password1' minLength={8} maxLength={16} required/>
+        //         <input type='password' name='password2' id='password2' minLength={8} maxLength={16} required/>
+        //         <button type='submit'>Submit</button>
+        //     </form>
+        //     <button onClick={()=>setForgotPass(true)}>Forgot Password</button>
+        // </div>
     )
 }
 
